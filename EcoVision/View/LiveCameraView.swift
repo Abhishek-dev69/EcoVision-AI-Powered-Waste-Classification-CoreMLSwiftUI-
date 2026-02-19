48e0cfe21a898eeb8ae07e31b5924baa98dc8178
Abhishek <abhishek12439525@gmail.com>
2026-02-19 08:55:39 +0530

    Some UI changes added the dark mode

--- EcoVision/View/LiveCameraView.swift
+++ EcoVision/View/LiveCameraView.swift
@@ -2,11 +2,11 @@
 //  LiveCameraView.swift
 //  EcoVision
 //
-//  Created by Abhishek on 17/02/26.
-//
 
 import SwiftUI
 import AVFoundation
+
+// MARK: SwiftUI Wrapper
 
 struct LiveCameraView: UIViewControllerRepresentable {
 
@@ -22,6 +22,9 @@ struct LiveCameraView: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: LiveCameraController, context: Context) {}
 }
 
+
+// MARK: UIKit Camera Controller
+
 class LiveCameraController: UIViewController {
 
     var viewModel: CameraViewModel!
@@ -29,50 +32,127 @@ class LiveCameraController: UIViewController {
     private let session = AVCaptureSession()
     private var previewLayer: AVCaptureVideoPreviewLayer!
 
+    private let sessionQueue = DispatchQueue(label: "camera.session.queue")
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupCamera()
-    }
 
-    private func setupCamera() {
-
-        session.sessionPreset = .high
-
-        guard let device = AVCaptureDevice.default(.builtInWideAngleCamera,
-                                                   for: .video,
-                                                   position: .back),
-              let input = try? AVCaptureDeviceInput(device: device)
-        else { return }
-
-        session.addInput(input)
-
-        let output = AVCaptureVideoDataOutput()
-
-        output.setSampleBufferDelegate(self,
-                                       queue: DispatchQueue(label: "live.camera"))
-
-        session.addOutput(output)
-
-        previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        previewLayer.frame = view.bounds
-        previewLayer.videoGravity = .resizeAspectFill
-
-        view.layer.addSublayer(previewLayer)
-
-        session.startRunning()
+        checkPermissionAndSetup()
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        previewLayer.frame = view.bounds
+
+        previewLayer?.frame = view.bounds
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        stopSession()
+    }
+
+    deinit {
+        stopSession()
+    }
+
+}
+
+
+// MARK: Camera Setup
+
+extension LiveCameraController {
+
+    private func checkPermissionAndSetup() {
+
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+
+        case .authorized:
+
+            setupCamera()
+
+        case .notDetermined:
+
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+
+                if granted {
+                    DispatchQueue.main.async {
+                        self?.setupCamera()
+                    }
+                }
+            }
+
+        default:
+            break
+        }
+    }
+
+    private func setupCamera() {
+
+        sessionQueue.async {
+
+            guard let device = AVCaptureDevice.default(
+                .builtInWideAngleCamera,
+                for: .video,
+                position: .back
+            ) else { return }
+
+            guard let input = try? AVCaptureDeviceInput(device: device) else {
+                return
+            }
+
+            if self.session.canAddInput(input) {
+                self.session.addInput(input)
+            }
+
+            let output = AVCaptureVideoDataOutput()
+
+            output.alwaysDiscardsLateVideoFrames = true
+
+            output.setSampleBufferDelegate(
+                self,
+                queue: DispatchQueue(label: "camera.frame.processing")
+            )
+
+            if self.session.canAddOutput(output) {
+                self.session.addOutput(output)
+            }
+
+            DispatchQueue.main.async {
+
+                self.previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+
+                self.previewLayer.videoGravity = .resizeAspectFill
+                self.previewLayer.frame = self.view.bounds
+
+                self.view.layer.addSublayer(self.previewLayer)
+            }
+
+            self.session.startRunning()
+        }
+    }
+
+    private func stopSession() {
+
+        sessionQueue.async {
+
+            if self.session.isRunning {
+                self.session.stopRunning()
+            }
+        }
     }
 }
 
+
+// MARK: Frame Processing
+
 extension LiveCameraController: AVCaptureVideoDataOutputSampleBufferDelegate {
 
-    func captureOutput(_ output: AVCaptureOutput,
-                       didOutput sampleBuffer: CMSampleBuffer,
-                       from connection: AVCaptureConnection) {
+    func captureOutput(
+        _ output: AVCaptureOutput,
+        didOutput sampleBuffer: CMSampleBuffer,
+        from connection: AVCaptureConnection
+    ) {
 
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
         else { return }
@@ -80,3 +160,4 @@ extension LiveCameraController: AVCaptureVideoDataOutputSampleBufferDelegate {
         viewModel.processLiveFrame(pixelBuffer)
     }
 }
+
